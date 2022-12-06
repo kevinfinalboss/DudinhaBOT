762ead8360ef8a59ce5c33aeb3599d36374edccd
Kevin Gomes <kevinmg50@gmail.com>
2022-12-06 16:13:04 -0300

    fix(ping.js): Changed some bot response messages

--- commands/Utilities/ping.js
+++ commands/Utilities/ping.js
@@ -1,28 +1,34 @@
 const Discord = require("discord.js")
 
 module.exports = {
-  name: "ping", // Coloque o nome do comando
-  description: "Veja o ping do bot.", // Coloque a descrição do comando
+  name: "ping",
+  description: "﹝Information﹞Shows Current Latency❗",
+  description_localizations: {"pt-BR":"﹝Informação﹞Ping da dudinha"},
   type: Discord.ApplicationCommandType.ChatInput,
 
   run: async (client, interaction) => {
-
+    const gateway = Date.now() - interaction.createdTimestamp
     let ping = client.ws.ping;
 
     let embed_1 = new Discord.EmbedBuilder()
     .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-    .setDescription(`Olá ${interaction.user}, meu ping está em \`calculando...\`.`)
-    .setColor("Random");
+    .setDescription(`Oi meu amor 🤤 ${interaction.user}, Meu Ping Está Em: \`calculando...\`.`)
+    .setFooter({ text: `Requisitado Por: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+    .setColor("FFFFFF");
 
     let embed_2 = new Discord.EmbedBuilder()
     .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
-    .setDescription(`Olá ${interaction.user}, meu ping está em \`${ping}ms\`.`)
-    .setColor("Random");
+    .setDescription(`**Ping: \`${ping}ms\` (\`MS\`)
+                       Gateway: \`${gateway}ms\` (\`MS\`)
+                       Shards: \`0\`**`)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setFooter({ text: `Requisitado Por: ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+    .setColor("FFFFFF");
 
     interaction.reply({ embeds: [embed_1] }).then( () => {
         setTimeout( () => {
             interaction.editReply({ embeds: [embed_2] })
-        }, 2000)
+        }, 3000)
     })
   }
 }
