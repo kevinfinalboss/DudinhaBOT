b005c474b23edb870963d01bd1ac59d38e3c34af
Kevin Gomes <kevinmg50@gmail.com>
2022-12-06 15:50:51 -0300

    fix(index.js): Remodeled the server structure for the most current version of the API

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
 const config = require("./config.json")
-
 const client = new Discord.Client({ 
   intents: [ 
 Discord.GatewayIntentBits.Guilds
@@ -12,13 +11,9 @@ module.exports = client
 client.on('interactionCreate', (interaction) => {
 
   if(interaction.type === Discord.InteractionType.ApplicationCommand){
-
       const cmd = client.slashCommands.get(interaction.commandName);
-
       if (!cmd) return interaction.reply(`Error`);
-
       interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-
       cmd.run(client, interaction)
 
    }
