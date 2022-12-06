83fa116da262252e44a373e654599f228d10b621
Kevin Gomes <kevinmg50@gmail.com>
2022-12-06 14:11:31 -0300

    feat(commands) created the command folder structure

---
+++ commands/CommandsUsers/ping.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "ping",
+
+    run: async(client, message, args) => {
+        let embed = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setAuthor({name: client.user.username, iconURL: client.user.displayAvatarURL()})
+        .setDescription(`Oi Amor 🤤 ${message.author}, seu ping está em: \`carregando....\`.`)
+
+        let embed2 = new Discord.EmbedBuilder()
+        .setColor("Random")
+        .setAuthor({name: client.user.username, iconURL: client.user.displayAvatarURL()})
+        .setDescription(`Oi Amor 🤤 ${message.author}, seu ping está em: \`${client.ws.ping}ms\`.`)
+
+        message.reply({embeds: [embed]}).then(msg => {
+            setTimeout( () => {
+                msg.edit({embeds: [embed2]})
+            }, 3000)
+        })
+
+
+    }
+}
