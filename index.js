3d6ccf196da9d9fe18679159fbf18dd052cc9ef2
Kevin Gomes <kevinmg50@gmail.com>
2022-12-31 02:43:46 -0300

    feat(index.js): added error handling, even with error the bot will keep running

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ client.on("ready", () => {
     console.log(`🔥 Estou online em ${client.user.username}! e pronta para mamar 🤤`)
 })
 
+client.on("ready", () => {
+  let canalPing = client.channels.cache.get(`${config.canalping}`); // Colocar o id do canal de ping
+  if (!canalPing) return console.log(`Canal de ping do bot não encontrado`);
+  canalPing.setName(`📡 Ping: Calculando...`);
+  setInterval(() => {
+    canalPing.setName(`📡 Ping: ${client.ws.ping}ms`);
+  }, 1000 * 60 * 4); 
+})
+
+client.on("ready", () => {
+  let users = client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)
+  const compact = users.toLocaleString("pt-BR", { notation: 'compact' });
+  let membro = client.channels.cache.get(`${config.canalmembros}`); // Colocar o id do canal de membros
+  if (!membro) return console.log(`Canal de membros do bot não encontrado`);
+  membro.setName(`📡 Membros: Calculando...`);
+  setInterval(() => {
+    membro.setName(`📡 Membros: ${compact}`);
+  }, 1000 * 60 * 4);
+})
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception: " + err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("ERRO possivelmente não tratada em: Promise ", promise, " motivo: ", reason.message);
+});
+
 
 client.slashCommands = new Discord.Collection()
 require('./handler')(client)
