30e0ac360fa357e7e15a83f87092b783815dcc82
Kevin Gomes <kevinmg50@gmail.com>
2022-12-31 02:44:31 -0300

    feat(alladmins): shows all the discord server admins

---
+++ commands/Admin/alladmins.js
@@ -0,0 +1,36 @@
+const { PermissionFlagsBits, EmbedBuilder, PermissionsBitField } = require("discord.js")
+
+module.exports = {
+
+    name: "deuses",
+    description: "Mostra todos os deuses do servidor",
+    permission: PermissionFlagsBits.Administrator,
+    dm: false,
+    exemple: "todos-admin",
+    category: "Administrador",
+    cooldown: 5,
+
+    async run (client, interaction, args) {
+
+        let strFilter = interaction.guild.members.cache.filter(member => member.permissions.has(PermissionsBitField.Flags.Administrator))
+        let strMap = strFilter.map(m => `> \`${m.user.id}\` - ${m.user}`).join("\n")
+
+        for (let i = 0; i < strMap.length; i += 1995) {
+
+            let strContent = strMap.substring(i, Math.floor(strMap.length, i + 1995));
+
+            let embed = new EmbedBuilder()
+            .setColor("White")
+            .setThumbnail(`${client.user.avatarURL()}`)
+            .setTitle("Lista de membros com a permissão de  \`Administrador\`")
+            .setDescription(`Número de membros com permissão : \`${strFilter.size}\``)
+            .addFields(
+                { name: `Lista :`, value: `${strContent}`}
+            )
+            .setTimestamp()
+
+            await interaction.reply({embeds: [embed]})
+        }
+            
+    }
+}
