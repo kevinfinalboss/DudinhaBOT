e41d25c687f9aec2740369a789c2f318155af0c5
kevinfinalboss <kevinmg50@gmail.com>
2023-01-29 21:07:48 -0300

    some changes

--- commands/Utilities/reminder.js
+++ commands/Utilities/reminder.js
@@ -2,28 +2,45 @@ const Discord = require("discord.js")
 const ms = require("ms")
 
 module.exports = {
-    name: 'reminder', 
-    description: 'lembrete', 
+    name: 'lembrete',
+    description: 'Me faça lembrar você de fazer algo!',
     type: Discord.ApplicationCommandType.ChatInput,
     options: [
         {
-            name: "time",
+            name: "tempo",
             description: "Selecione o tempo para o lembrete.",
             type: Discord.ApplicationCommandOptionType.String,
             required: true
-        }
+          },
+          {
+            name: "motivo",
+            description: "Selecione o tempo para o lembrete.",
+            type: Discord.ApplicationCommandOptionType.String,
+            required: true
+        },
     ],
 
     run: async(client, interaction) => {
-        const time = interaction.options.getString("time")
-        const timerC = ms(time)
-        const timerR = ~~((Date.now() + timerC) / 1000)
+        const motivo = interaction.options.getString("motivo")
+        const tempo = interaction.options.getString("tempo")
+        const tempoC = ms(tempo)
+        const tempoR = ~~((Date.now() + tempoC) / 1000)
 
-        await interaction.reply(`**Olá ${interaction.user.username}!** lembrete setado com sucesso para acabar <t:${timerR}:R>`)
+            const embedS = new Discord.EmbedBuilder()
+                .setColor("Green")
+                .setDescription(`**Olá, ${interaction.user}!** Lembrete setado com sucesso para acabar <t:${tempoR}:R>.`)
+                .setFooter({ text: `Comando requisitado por: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ format: "png" }) });
 
+            const embedL = new Discord.EmbedBuilder()
+                .setColor("Green")
+                .setDescription(`**Opa, ${interaction.user}!** Vim alertar-te sobre seu lembrete! Você pediu para ser lembrado sobre: \`${motivo}\`.`)
+                .setFooter({ text: `Comando requisitado por: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ format: "png" }) });
+      
+      
+        interaction.reply({ embeds: [embedS] })
         setTimeout(async function () {
-            await interaction.followUp(`Oi ${interaction.user}, só vim avisar sobre seu lembrete mesmo...`)
-        }, ms (time))
+        await interaction.followUp({ embeds: [embedL] })
+        }, ms (tempo))
 
     }
 }
